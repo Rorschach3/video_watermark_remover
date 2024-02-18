f5df0841054a9184e01d167d8f068640c5ef8b2d
rorschach3 <daniel_fhernandez@yahoo.com>
2024-02-18 03:32:14 -0800

    update

---
+++ README.md
@@ -0,0 +1,13 @@
+---
+runme:
+  id: 01HPXYZDSGHDRSBE93CVBPJZQ8
+  version: v3
+---
+
+### Video Watermark Remover
+
+
+### Introduction
+
+This python program will remove watermarks from videos using mmfpeg 
+video upload implementation and x,y coordinate, width,height mask filter capability will be added to allow user to shade in area of watermark to be removed.
--- mmfpeg.py
+++ mmfpeg.py
@@ -2,17 +2,22 @@ import subprocess
 
 
 def remove_logo(input_file, output_file):
-    delogo_filter = f'delogo=x={x_horizontal}:y={y_vertical}:w={width}:h={height}'
+    delogo_filter = f'''delogo=
+    x={x_horizontal}:
+    y={y_vertical}:
+    w={width}:
+    h={height}'''
     command = ['ffmpeg', '-i', input_file, '-vf', delogo_filter, output_file]
     subprocess.run(command)
 
 
 # Replace 'input_file_path' and 'output_file_path' with your desired file paths
-input_file_path = '/mnt/g/downloads/instagram.mp4'
+input_file_path = 'instagram.mp4'
 filename = input('File name: ')
-output_file_path = f'{filename}.mp4'
+output_file_path = f'./output/{filename}.mp4'
 x_horizontal = input('Enter the x coordinate of the logo: ')
 y_vertical = input('Enter the y coordinate of the logo: ')
 width = input('Enter the width of the logo: ')
 height = input('Enter the height of the logo: ')
 remove_logo(input_file_path, output_file_path)
+file = input('Name of file:  ','.mp4')
